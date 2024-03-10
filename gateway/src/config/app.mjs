b8b73ffe5996232d12f2da73315a284be1ea83f9
Jeremy <jeremy.dryden@gmail.com>
2024-03-09 23:26:57 -0500

    working on adding auth0

--- gateway/src/config/app.mjs
+++ gateway/src/config/app.mjs
@@ -1,5 +1,20 @@
 import express from "express";
+import jwt from "express-jwt";
+import jwksClient from "jwks-rsa";
 
 const app = express();
+
+const jwtCheck = jwt({
+  secret: jwksClient.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwkRequestsPerMinute: 5,
+    jwksUri: `${process.env.AUTH0_ISSUER}.well-known/jwks.json`,
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: process.env.AUTH0_ISSUER,
+  algorithms: ["RS256"],
+  credentialsRequired: false,
+});
 
 export default app;
