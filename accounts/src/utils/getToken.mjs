1677749e987ac77ef0a7f828830a8c074c8722c1
Jeremy <jeremy.dryden@gmail.com>
2024-04-17 22:54:41 -0400

    got a working script for token generation

--- accounts/src/utils/getToken.mjs
+++ accounts/src/utils/getToken.mjs
@@ -1,4 +1,5 @@
 import util from "util";
+
 import request from "request";
 
 const requestPromise = util.promisify(request);
@@ -7,14 +8,12 @@ async function getToken(username, password) {
   const options = {
     method: "POST",
     url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-    },
+    headers: { "content-type": "application/x-www-form-urlencoded" },
     form: {
       audience: process.env.AUTH0_AUDIENCE,
       client_id: process.env.AUTH0_CLIENT_ID_GRAPHQL,
       client_secret: process.env.AUTH0_CLIENT_SECRET_GRAPHQL,
-      graph_type: "http://auth0.com/oauth/grant-type/password-realm",
+      grant_type: "http://auth0.com/oauth/grant-type/password-realm",
       password,
       realm: "Username-Password-Authentication",
       scope: "openid",
